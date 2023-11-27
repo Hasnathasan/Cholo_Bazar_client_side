200867d8a5aab806b2e85d0b025e2c79cff25aec
Hasnathasan <hasnatoooooooo@gmail.com>
2023-11-27 23:39:16 +0600

    Add New Product Page created

--- src/UserDashboard/DashboardOfAdmin/AddNewProduct.jsx
+++ src/UserDashboard/DashboardOfAdmin/AddNewProduct.jsx
@@ -1,31 +1,142 @@
-import { Input } from "@nextui-org/react";
+import { Button, Input, Select, SelectItem, Textarea } from "@nextui-org/react";
+import { useState } from "react";
+import { FaPlus } from "react-icons/fa";
 
 
 
 const AddNewProduct = () => {
+    const [totalSpecification, setTotalSpecification] = useState([1])
     return (
-        <div className="w-full px-4 md:px-8">
-            <div className="bg-white p-5 rounded-xl grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 shadow-sm w-full">
-            <Input size={"sm"} type="text" label="Product Name" />
-            <Input size={"sm"} type="text" label="Product Name" />
-            <Input size={"sm"} type="text" label="Product Name" />
-            <Input size={"sm"} type="text" label="Product Name" />
-            <Input size={"sm"} type="text" label="Product Name" />
-            <Input size={"sm"} type="text" label="Product Name" />
-            <Input size={"sm"} type="text" label="Product Name" />
-            <Input size={"sm"} type="text" label="Product Name" />
-            <Input size={"sm"} type="text" label="Product Name" />
-            <Input size={"sm"} type="text" label="Product Name" />
-            <Input size={"sm"} type="text" label="Product Name" />
-            <Input size={"sm"} type="text" label="Product Name" />
-            <Input size={"sm"} type="text" label="Product Name" />
-            <Input size={"sm"} type="text" label="Product Name" />
-            <Input size={"sm"} type="text" label="Product Name" />
-            <Input size={"sm"} type="text" label="Product Name" />
-            <Input size={"sm"} type="text" label="Product Name" />
-            <Input size={"sm"} type="text" label="Product Name" />
-            <Input size={"sm"} type="text" label="Product Name" />
-            <Input size={"sm"} type="text" label="Product Name" />
+        <div className="w-full px-3 md:px-8">
+            <div className="bg-white p-4 md:p-10 rounded-xl shadow-sm w-full ">
+            <h2 className="text-3xl font-semibol underline leading-[50px] text-gray-900 mb-4">Product Details:</h2>
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-8">
+            <Input
+          type="text"
+          label="Name of your Product"
+          placeholder="Product name"
+          variant="underlined"
+          labelPlacement="outside"
+        />
+           
+          <Input
+        type="file"
+        label="Choose Images"
+        placeholder=" "
+        multiple
+        variant="underlined"
+        labelPlacement="outside"
+      />
+            <Input
+          type="number"
+          label="Real Price"
+          placeholder="Tk. 00"
+          variant="underlined"
+          labelPlacement="outside"
+        />
+            <Input
+          type="number"
+          label="Price after discount"
+          placeholder="Tk. 00"
+          variant="underlined"
+          labelPlacement="outside"
+        />
+        <Select
+      label="Select main category"
+      placeholder="Select an category"
+      labelPlacement="outside"
+      variant="underlined"
+      className="max-w-xs"
+    >
+      <SelectItem key={"Electronics"}>Electronics</SelectItem>
+      <SelectItem key={"kidsZone"}>Kids Zone</SelectItem>
+      <SelectItem key={"personal-Care"}>Personal-Care</SelectItem>
+      <SelectItem key={"daily-needs"}>Daily-needs</SelectItem>
+    </Select>
+        <Select
+      label="Select Category"
+      placeholder="Select an category"
+      labelPlacement="outside"
+      variant="underlined"
+      className="max-w-xs"
+    >
+      <SelectItem key={"Electronics"}>Electronics</SelectItem>
+      <SelectItem key={"kidsZone"}>Kids Zone</SelectItem>
+      <SelectItem key={"personal-Care"}>Personal-Care</SelectItem>
+      <SelectItem key={"daily-needs"}>Daily-needs</SelectItem>
+    </Select>
+    <Input
+          type="number"
+          label="Product rating"
+          placeholder="rating"
+          variant="underlined"
+          labelPlacement="outside"
+        />
+        <Input
+          type="text"
+          label="Country of origin"
+          placeholder="Origin country name"
+          variant="underlined"
+          labelPlacement="outside"
+        />
+            </div>
+            <h2 className="text-xl font-semibold underline leading-[50px] text-gray-900 my-5">Brand Details:</h2>
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-6">
+            <Input
+          type="text"
+          label="Brand Name"
+          placeholder="Name of brand"
+          variant="underlined"
+          labelPlacement="outside"
+        />
+            <Input
+          type="text"
+          label="Brand Name"
+          placeholder="Name of brand"
+          variant="underlined"
+          labelPlacement="outside"
+        />
+            </div>
+            <h2 className="text-xl font-semibold underline leading-[50px] text-gray-900 my-5">Description&apos;s of Product and Brand:</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <Textarea
+        label="Summery of Product"
+        variant="underlined"
+        placeholder="Enter your Summery"
+      />
+            <Textarea
+        label="Description of the Brand"
+        variant="underlined"
+        placeholder="Enter brand description"
+      />
+            </div>
+            <h2 className="text-xl font-semibold underline leading-[50px] text-gray-900 my-5">Product specifications:</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 relative gap-10">
+            <Button onClick={() => setTotalSpecification([...totalSpecification, totalSpecification.length + 1])} isIconOnly color="primary" className="text-white absolute -top-4 right-3" variant="solid" aria-label="Take a photo">
+        <FaPlus></FaPlus>
+      </Button>
+           {
+            totalSpecification?.map(num =>  <div key={num} className="flex justify-start items-center">
+            <Input
+          type="text"
+          label="Property"
+          placeholder="Name of property"
+          variant="bordered"
+          radius="none"
+          labelPlacement="outside"
+        />
+            <Input
+          type="text"
+          label="Value"
+          placeholder="Name of value"
+          variant="bordered"
+          radius="none"
+          labelPlacement="outside"
+        />
+            </div>)
+           }
+            </div>
+
             </div>
         </div>
     );
--- src/UserDashboard/DashboardOfAdmin/ManageOrders.jsx
+++ src/UserDashboard/DashboardOfAdmin/ManageOrders.jsx
@@ -1,10 +1,111 @@
 
+import { Button, ButtonGroup, Chip, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Input, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, User } from "@nextui-org/react";
+
+import img from '../../../public/user.png'
+import axios from "axios";
+import { FaArrowDown, FaPlus, FaSearch } from "react-icons/fa";
+import UseProductsBySecondaryCategory from "../../Hooks/UseProductsBySecondaryCategory";
 
 const ManageOrders = () => {
+    const [products, isProductsLoading] = UseProductsBySecondaryCategory({
+        category: "iron"
+      });
+      if(isProductsLoading){
+        return <h1>Loading............</h1>
+      }
     return (
-        <div>
-            <h2 className="text-center text-4xl text-blue-500">This is Manage Orders</h2>
-        </div>
+        <div className="overflow-x-auto w-full md:w-[80%]">
+        <div className="flex flex-col  gap-4">
+    <div className="flex justify-between p-5 bg-white rounded-xl gap-3 items-end">
+      <Input
+        isClearable
+        className="w-full sm:max-w-[44%]"
+        placeholder="Search by name..."
+        startContent={<FaSearch></FaSearch>}
+      />
+      <div className="flex gap-3">
+        <Dropdown>
+          <DropdownTrigger className="hidden sm:flex">
+            <Button endContent={<FaArrowDown></FaArrowDown>} variant="flat">
+              Status
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu
+            disallowEmptySelection
+            aria-label="Table Columns"
+            closeOnSelect={false}
+            selectedKeys={["data"]}
+            selectionMode="multiple"
+          >
+              <DropdownItem key={"data"}  className="capitalize">
+                Data
+              </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <Dropdown>
+          <DropdownTrigger className="hidden sm:flex">
+            <Button endContent={<FaArrowDown></FaArrowDown>} variant="flat">
+              Columns
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu
+            disallowEmptySelection
+            aria-label="Table Columns"
+            closeOnSelect={false}
+            selectedKeys={["hi"]}
+            selectionMode="multiple"
+          >
+              <DropdownItem key={"hi"} className="capitalize">
+                Hi
+              </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <Button color="primary" endContent={<FaPlus></FaPlus>}>
+          Add New
+        </Button>
+      </div>
+    </div>
+      <span className="text-gray-600 mb-2">Total {products?.length} users</span>
+  </div>
+         <Table aria-label="Example table with custom cells">
+  <TableHeader>
+      <TableColumn >
+        Name
+      </TableColumn>
+      <TableColumn >
+        Price
+      </TableColumn>
+      <TableColumn >
+        Category
+      </TableColumn>
+      <TableColumn >
+        Main Category
+      </TableColumn>
+      <TableColumn >
+        Actions
+      </TableColumn>
+  </TableHeader>
+  <TableBody >
+      {
+        products?.map(product => <TableRow key={product._id} className="hover:!bg-gray-100 duration-100">
+            <TableCell>
+            {product.specification.Title.slice(0, 30)}....
+            </TableCell>
+            <TableCell>{product?.price?.discounted_price}</TableCell>
+            <TableCell>
+            {product?.secondary_category}
+            </TableCell>
+            <TableCell>
+            {product?.main_category}
+            </TableCell>
+            <TableCell>
+            {product?.main_category}
+            </TableCell>
+          </TableRow>)
+      }
+  </TableBody>
+</Table>
+    </div>
     );
 };
 
