5fa80e949cae00a9d4bf269e8f9a1d709f5ab22e
Hasnathasan <hasnatoooooooo@gmail.com>
2023-12-22 17:42:29 +0600

    Changes

--- src/Components/HoverProductCart/HoverProductCard.jsx
+++ src/Components/HoverProductCart/HoverProductCard.jsx
@@ -3,19 +3,7 @@ import "./HoverProductCard.css";
 const HoverProductCard = ({ product }) => {
   console.log(product);
   const {
-    _id,
     images,
-    price,
-    rating,
-    reviews,
-    number_of_ratings,
-    number_of_reviews,
-    brand_info,
-    tags,
-    secondary_category,
-    super_deal,
-    main_category,
-    specification,
   } = product;
   return (
     <div className="w-[270px]">
